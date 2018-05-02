0ff458a0d9cab686d3ac6f3b88faf740037fc2e3
RobinMarechal <robin.marechal-2@etu.univ-tours.fr>
2018-05-02 04:02:49 +0200

    new version

--- src/routes/rest-api-routes.php
+++ src/routes/rest-api-routes.php
@@ -2,8 +2,6 @@
 
 $routePrefix = config("rest.route_prefix");
 
-//Route::get("$routePrefix/test", "\RobinMarechal\RestApi\Controllers\RestControllerCo@test");
-
 Route::prefix($routePrefix)->group(function () {
     Route::any('{resource}/{id?}/{relation?}/{relationId?}', "RobinMarechal\RestApi\Controllers\RestController@dispatch");
 });
